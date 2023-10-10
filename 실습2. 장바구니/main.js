e43389ca062bf9b04386864fbaeb05e837289711
Lysepten <141825173+Lysepten@users.noreply.github.com>
2023-10-10 21:40:33 +0900

    Add files via upload

--- 실습2. 장바구니/main.js
+++ 실습2. 장바구니/main.js
@@ -9,23 +9,48 @@
 // let quan = document.getElementsByClassName('quantity');
 // console.log(quan);
 
-let btn = document.querySelector('.addCart');
-btn.addEventListener('click', addToCart);
 
-function addToCart(event) {
+
+// let btn = document.querySelector('.addCart');
+// document.querySelector('.addCart');
+
+let products = document.querySelectorAll(".product");
+
+// let pro = (".product").length;
+// console.log(pro);
+
+for (let i = 0; i < products.length; i++) {
+    let product = products[i]
+    let addBtn = product.querySelector('.addCart')
+    addBtn.addEventListener("click", addCart);
+}
+
+// btn.addEventListener('click', addToCart);
+
+function addCart(event) {
     let product = event.target.parentElement;
-    console.log(product);
 
     let a = product.querySelector('span').textContent;
     let b = product.querySelector('.price').textContent;
-    let c = product.querySelector('.quantity').textContent;
+    let c = parseInt(product.querySelector('.quantity').value);
     let bb = b.slice(0, -1);
+
+    if (c > 0) {
+        let list = document.createElement("li"); 
+        list.textContent =
+        a + " [" + c + "개] : " + bb * c + "원";
+
+        // list.textContent = a + bb * c;
+        document.querySelector("ul").appendChild(list);
+    }
+
+    let totalpe = document.querySelector(".total2");
+    let totalprice = parseInt(totalpe.textContent.slice(0, -1));
+    totalprice += bb * c;
+    totalpe.textContent = totalprice + "원";
     
 
-    let list = document.createElement("li"); 
-    list.textContent = a + bb*c;
-    document.querySelector("ul").appendChild(list);
-    console.log(list);
+
 }
 // btn.addEventListener('click', event1)
 // function event1() {
